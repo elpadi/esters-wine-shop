bcaaf7f6eacb02783fc6e4ddf29fdd724bc79623
carlos (minimal) <padi_05@yahoo.com>
2017-12-03 19:45:57 -0500

    Updated WooCommerce templates

--- wp-content/themes/esters/woocommerce/content-product.php
+++ wp-content/themes/esters/woocommerce/content-product.php
@@ -13,7 +13,7 @@
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @author  WooThemes
  * @package WooCommerce/Templates
- * @version 2.6.1
+ * @version 3.0.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -27,7 +27,7 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	return;
 }
 ?>
-<li <?php post_class('col-xs-12 col-sm-6 col-md-4 product-single'); ?>>
+<li <?php post_class(); ?>>
 	<?php
 	/**
 	 * woocommerce_before_shop_loop_item hook.
