7af1e31c830e088c2429ceef73233618d7ab1c22
Carlos Padilla <padi_05@yahoo.com>
2018-08-17 16:51:05 -0400

    Removed the shop online pic

--- wp-content/themes/esters/template-shop-landing.php
+++ wp-content/themes/esters/template-shop-landing.php
@@ -21,24 +21,12 @@ extract(get_field('shop_online'));
                     </div><!-- entry -->
                 </div><!-- col -->
 				<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-					<div class="entry">
-						<div class="row">
-							<div class="col-xs-12 col-sm-12 col-md-7 col-lg-8">
-								<h3><?= $shop_online_title; ?></h3>
-								<?= $shop_online_copy; ?>
-								<br>
-								<ul class="list-inline"><li>
-									<a class="btn btn-outline" href="<?= esc_url(home_url('/')); ?>shop/wines/">Shop Now!</a>
-								</li></ul>
-							</div>
-							<div class="col-xs-12 col-sm-12 col-md-5 col-lg-4 hidden-sm hidden-xs">
-								<figure class="img-holder">
-                                	<?php // get the thumbnail id using the queried category term_id
-									$wines_id = get_woocommerce_term_meta( 48, 'thumbnail_id', true );  ?>
-								   <img src="<?= $image = wp_get_attachment_url( $wines_id ); ?>" class="img-responsive" alt="Wine">
-								</figure>
-                            </div><!-- col  -->
-                        </div><!-- row -->
+                    <div class="entry">
+						<h3><?= $shop_online_title; ?></h3>
+						<?= $shop_online_copy; ?>
+                        <ul class="list-inline">
+							<li><a class="btn btn-outline" href="<?= esc_url(home_url('/')); ?>shop/wines/">Shop Now!</a></li>
+                        </ul>
                     </div><!-- entry -->
                 </div><!-- col -->
             </div><!-- row -->
