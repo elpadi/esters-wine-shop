ff5352031b101a9356be6f7fc4c24d9bccf6722b
Carlos Padilla <padi_05@yahoo.com>
2018-08-23 16:47:08 -0400

    Shop landing custom fields

--- wp-content/themes/esters/lib/store.php
+++ wp-content/themes/esters/lib/store.php
@@ -133,3 +133,142 @@ add_filter('woocommerce_get_price_html', function($html, $product) {
 	return $html;
 }, 10, 2);
 
+if( function_exists('acf_add_local_field_group') ):
+
+acf_add_local_field_group(array(
+	'key' => 'group_5b772b6970d02',
+	'title' => 'Shop Landing',
+	'fields' => array(
+		array(
+			'key' => 'field_5b772b743e8ff',
+			'label' => 'Wine Clubs',
+			'name' => 'wine_clubs',
+			'type' => 'group',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'layout' => 'block',
+			'sub_fields' => array(
+				array(
+					'key' => 'field_5b772cb455aa6',
+					'label' => 'Wine Clubs Title',
+					'name' => 'wine_clubs_title',
+					'type' => 'text',
+					'instructions' => '',
+					'required' => 0,
+					'conditional_logic' => 0,
+					'wrapper' => array(
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'default_value' => '',
+					'placeholder' => '',
+					'prepend' => '',
+					'append' => '',
+					'maxlength' => '',
+				),
+				array(
+					'key' => 'field_5b772b9f3e900',
+					'label' => 'Wine Clubs Copy',
+					'name' => 'wine_clubs_copy',
+					'type' => 'wysiwyg',
+					'instructions' => '',
+					'required' => 0,
+					'conditional_logic' => 0,
+					'wrapper' => array(
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'default_value' => '',
+					'tabs' => 'all',
+					'toolbar' => 'full',
+					'media_upload' => 1,
+					'delay' => 0,
+				),
+			),
+		),
+		array(
+			'key' => 'field_5b772bec3e901',
+			'label' => 'Shop Online',
+			'name' => 'shop_online',
+			'type' => 'group',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'layout' => 'block',
+			'sub_fields' => array(
+				array(
+					'key' => 'field_5b772ccf55aa7',
+					'label' => 'Shop Online Title',
+					'name' => 'shop_online_title',
+					'type' => 'text',
+					'instructions' => '',
+					'required' => 0,
+					'conditional_logic' => 0,
+					'wrapper' => array(
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'default_value' => '',
+					'placeholder' => '',
+					'prepend' => '',
+					'append' => '',
+					'maxlength' => '',
+				),
+				array(
+					'key' => 'field_5b772bec3e902',
+					'label' => 'Shop Online Copy',
+					'name' => 'shop_online_copy',
+					'type' => 'wysiwyg',
+					'instructions' => '',
+					'required' => 0,
+					'conditional_logic' => 0,
+					'wrapper' => array(
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'default_value' => '',
+					'tabs' => 'all',
+					'toolbar' => 'full',
+					'media_upload' => 1,
+					'delay' => 0,
+				),
+			),
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'page',
+				'operator' => '==',
+				'value' => '859',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => array(
+		0 => 'the_content',
+	),
+	'active' => 1,
+	'description' => '',
+));
+
+endif;
