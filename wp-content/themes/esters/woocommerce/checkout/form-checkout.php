097e9229ca423d1c9d2b466a7b529cf40ebc514b
carlos (minimal) <padi_05@yahoo.com>
2018-12-11 18:39:44 -0500

    Updated checkout templates

--- wp-content/themes/esters/woocommerce/checkout/form-checkout.php
+++ wp-content/themes/esters/woocommerce/checkout/form-checkout.php
@@ -10,61 +10,60 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see 	    https://docs.woocommerce.com/document/template-structure/
- * @author 		WooThemes
- * @package 	WooCommerce/Templates
- * @version     2.3.0
+ * @see https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce/Templates
+ * @version 3.5.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-wc_print_notices();
-
 do_action( 'woocommerce_before_checkout_form', $checkout );
 
-// If checkout registration is disabled and not logged in, the user cannot checkout
-if ( ! $checkout->enable_signup && ! $checkout->enable_guest_checkout && ! is_user_logged_in() ) {
-	echo apply_filters( 'woocommerce_checkout_must_be_logged_in_message', __( 'You must be logged in to checkout.', 'woocommerce' ) );
+// If checkout registration is disabled and not logged in, the user cannot checkout.
+if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_required() && ! is_user_logged_in() ) {
+	echo esc_html( apply_filters( 'woocommerce_checkout_must_be_logged_in_message', __( 'You must be logged in to checkout.', 'woocommerce' ) ) );
 	return;
 }
 
 ?>
 
-<div class="container">
-    <form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url( wc_get_checkout_url() ); ?>" enctype="multipart/form-data">
-    <div class="row">
-        <?php if ( sizeof( $checkout->checkout_fields ) > 0 ) : ?>
+<form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url( wc_get_checkout_url() ); ?>" enctype="multipart/form-data">
 
-            <?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
+	<div class="row">
 
-            <div class="col-xs-12 col-sm-6" id="customer_details">
+		<?php if ( $checkout->get_checkout_fields() ) : ?>
 
-                    <?php do_action( 'woocommerce_checkout_billing' ); ?>
+		<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
 
+		<div class="col-xs-12 col-sm-6" id="customer_details">
 
+			<?php do_action( 'woocommerce_checkout_billing' ); ?>
+			<?php do_action( 'woocommerce_checkout_shipping' ); ?>
 
-                    <?php do_action( 'woocommerce_checkout_shipping' ); ?>
+			<?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
 
-            </div>
+		</div>
 
-            <?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
+		<?php endif; ?>
 
-        <?php endif; ?>
-        <div class="col-xs-12 col-sm-6" id="order_details">
+		<div class="col-xs-12 col-sm-6" id="order_details">
 
-            <h3 id="order_review_heading"><?php _e( 'Your order', 'woocommerce' ); ?></h3>
+			<h3 id="order_review_heading"><?php esc_html_e( 'Your order', 'woocommerce' ); ?></h3>
 
-            <?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
+			<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
 
-            <div id="order_review" class="woocommerce-checkout-review-order">
-                <?php do_action( 'woocommerce_checkout_order_review' ); ?>
-            </div>
+			<div id="order_review" class="woocommerce-checkout-review-order">
+				<?php do_action( 'woocommerce_checkout_order_review' ); ?>
+			</div>
 
-            <?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
-        </div>
-    </div>
-    </form>
-</div>
+			<?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
+
+		</div>
+
+	</div>
+
+</form>
+
 <?php do_action( 'woocommerce_after_checkout_form', $checkout ); ?>
--- wp-content/themes/esters/woocommerce/checkout/form-coupon.php
+++ wp-content/themes/esters/woocommerce/checkout/form-coupon.php
@@ -10,37 +10,33 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see     https://docs.woocommerce.com/document/template-structure/
- * @author  WooThemes
+ * @see https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce/Templates
- * @version 2.2
+ * @version 3.4.4
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
+defined( 'ABSPATH' ) || exit;
 
-if ( ! wc_coupons_enabled() ) {
+if ( ! wc_coupons_enabled() ) { // @codingStandardsIgnoreLine.
 	return;
 }
 
-if ( empty( WC()->cart->applied_coupons ) ) {
-	$info_message = apply_filters( 'woocommerce_checkout_coupon_message', __( 'Have a coupon?', 'woocommerce' ) . ' <a href="#" class="showcoupon">' . __( 'Click here to enter your code', 'woocommerce' ) . '</a>' );
-	wc_print_notice( $info_message, 'notice' );
-}
 ?>
-
-<div class="container">
-<form class="checkout_coupon" method="post" style="display:none">
-
-    <div class="row">
-        <div class="col-xs-12 col-sm-8 col-md-6 col-md-offset-2">
-            <input type="text" name="coupon_code" class="form-control input-text" placeholder="<?php esc_attr_e( 'Coupon code', 'woocommerce' ); ?>" id="coupon_code" value="" />
-        </div>
-        <div class="col-xs-12 col-sm-4 col-md-2">
-            <input type="submit" class="button" name="apply_coupon" value="<?php esc_attr_e( 'Apply Coupon', 'woocommerce' ); ?>" />
-        </div>
-    </div>
-
-</form>
+<div class="woocommerce-form-coupon-toggle">
+	<?php wc_print_notice( apply_filters( 'woocommerce_checkout_coupon_message', __( 'Have a coupon?', 'woocommerce' ) . ' <a href="#" class="showcoupon">' . __( 'Click here to enter your code', 'woocommerce' ) . '</a>' ), 'notice' ); ?>
 </div>
+
+<form class="checkout_coupon woocommerce-form-coupon" method="post" style="display:none">
+
+	<p><?php esc_html_e( 'If you have a coupon code, please apply it below.', 'woocommerce' ); ?></p>
+
+	<p class="form-row form-row-first">
+		<input type="text" name="coupon_code" class="input-text" placeholder="<?php esc_attr_e( 'Coupon code', 'woocommerce' ); ?>" id="coupon_code" value="" />
+	</p>
+
+	<p class="form-row form-row-last">
+		<button type="submit" class="button" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?>"><?php esc_html_e( 'Apply coupon', 'woocommerce' ); ?></button>
+	</p>
+
+	<div class="clear"></div>
+</form>
