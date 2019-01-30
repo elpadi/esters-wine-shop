da7bfa52e0b69947690fdb7fe36141ec9f057877
carlos (minimal) <padi_05@yahoo.com>
2019-01-29 22:44:10 -0500

    Disabled gift wrap when local pickup

--- wp-content/themes/esters/assets/scripts/main.js
+++ wp-content/themes/esters/assets/scripts/main.js
@@ -316,21 +316,30 @@ $('select').each(function(){
         // JavaScript to be fired on the about us page
       }
     },
-     'checkout': {
+    'checkout': {
       init: function() {
-          
-            var ckbox = $('#custom_checkbox');
-
-    $('.giftcheckbox input').on('click',function () {
-        if (ckbox.is(':checked')) {
-           $('.gift_recipient_wrapper').slideDown(700);
-        } else {
-             $('.gift_recipient_wrapper').slideUp(700);
-        }
-    });
-          
-      }
-     },
+		  var ckbox = $('#custom_checkbox'), gr = $('#gift_recipient');
+		  $('.giftcheckbox input').on('click',function () {
+			  if (ckbox.is(':checked')) {
+				  gr.slideDown(700);
+			  } else {
+				  gr.slideUp(700);
+			  }
+		  });
+		  // disable the gift checkbox if local pickup
+		  var f = $(document.forms.checkout).on('change', function() {
+			  var rad = this.querySelector('.shipping_method:checked');
+			  if (rad) {
+				  ckbox.prop('disabled', rad.id.indexOf('local_pickup') != -1);
+				  if (rad.id.indexOf('local_pickup') != -1) {
+					  ckbox.prop('checked', false);
+					  gr.slideUp(700);
+				  }
+			  }
+		  });
+		  
+	  }
+	 },
       
       
       
