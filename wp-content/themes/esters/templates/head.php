42333387e9d373eb2ca665a848c831cb74f8e2e7
carlos (minimal) <padi_05@yahoo.com>
2019-11-12 11:59:04 -0500

    Favicon update

--- wp-content/themes/esters/templates/head.php
+++ wp-content/themes/esters/templates/head.php
@@ -42,24 +42,15 @@ content="Esters, Santa Monica, Calif." />
 <meta property="og:description" content="Esters is the newest member of the Rustic Canyon Family of restaurants – a partnership between Josh Loeb & Zoe Nathan and Rustic Canyon restaurant’s wine director, Kathryn Coker, and her husband, Tug." />
 
     
-<link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri();?>/fav/apple-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri();?>/fav/apple-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri();?>/fav/apple-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri();?>/fav/apple-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri();?>/fav/apple-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri();?>/fav/apple-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri();?>/fav/apple-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri();?>/fav/apple-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri();?>/fav/apple-icon-180x180.png">
-<link rel="icon" type="image/png" sizes="192x192"  href="<?php echo get_template_directory_uri();?>/fav/android-icon-192x192.png">
-<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri();?>/fav/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="96x96" href="<?php echo get_template_directory_uri();?>/fav/favicon-96x96.png">
-<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri();?>/fav/favicon-16x16.png">
-<link rel="manifest" href="<?php echo get_template_directory_uri();?>/fav/manifest.json">
-<meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
-<meta name="theme-color" content="#ffffff">    
-    
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#bf9d3e">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
+
+
  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDwthfpVNhd5zzCn5Q_nbMHTcIO9tXGdIY"
 type="text/javascript"></script>
 </head>
