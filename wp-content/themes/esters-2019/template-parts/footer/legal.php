428de29f6a7f9bccf003a2862143d24a567ffec2
carlos (minimal) <padi_05@yahoo.com>
2019-11-27 19:35:43 -0500

    Responsive styles fixes

--- wp-content/themes/esters-2019/template-parts/footer/legal.php
+++ wp-content/themes/esters-2019/template-parts/footer/legal.php
@@ -2,9 +2,10 @@
 global $themeData;
 $legal = current(ThemeLib\Theme::instance()->get('customizerSections', 'legal')->getValues());
 ?>
-<p id="legal__copyright"><?= str_replace('%year%', date('Y'), $legal['copyright']); ?></p>
-<ul id="legal__pages" class="hlist">
-	<?php foreach (['tnc','privacy','tou'] as $key): $page = $legal[$key]; if (!$page) continue; ?>
-	<li><a href="<?= get_permalink($page); ?>"><?= $page->post_title; ?></a></li>
-	<?php endforeach; ?>
-</ul>
+<p id="legal__copyright">
+	<?= str_replace('%year%', date('Y'), $legal['copyright']); ?>
+	<?= implode(' <span class="separator">&bull;</span> ', array_filter(Functional\map(['tnc','privacy','tou'], function($key) use ($legal) {
+		$page = $legal[$key];
+		return $page ? sprintf('<a href="%s">%s</a>', get_permalink($page), $page->post_title) : '';
+	}))); ?>
+</p>
