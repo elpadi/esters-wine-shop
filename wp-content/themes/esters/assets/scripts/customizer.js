17edf32d158ec305b1294a5f1830ab04cfddad60
carlos (minimal) <padi_05@yahoo.com>
2019-12-10 21:24:41 -0500

    Removed old theme

--- wp-content/themes/esters/assets/scripts/customizer.js
+++
@@ -1,8 +0,0 @@
-(function($) {
-  // Site title
-  if (('wp' in window) && ('customize' in wp)) wp.customize('blogname', function(value) {
-    value.bind(function(to) {
-      $('.brand').text(to);
-    });
-  });
-})(jQuery);
--- wp-content/themes/esters/templates/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="content-info" role="contentinfo">
- <div class="container">
-        <p>Copyright © <?php echo date("Y") ?> - Esters Wine Shop, All Rights Reserved. <a href="<?php echo site_url();?>/terms-and-conditions/">Terms and Conditions</a></p>
-      </div>
-</footer>
